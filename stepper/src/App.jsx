3a33bd6c5d5b60d275c83b7d11cba4693ed589de
pchimurkar <priyachimurkar95@gmail.com>
2024-05-15 22:52:58 +0530

    update style and code

--- stepper/src/App.jsx
+++ stepper/src/App.jsx
@@ -6,19 +6,19 @@ function App() {
   const CHECKOUT_STEPS =[
     {
       name :"Customer Info",
-      Component:()=><div>Provide your contact details.</div>
+      Component:()=><div className="text-comp">Provide your contact details.</div>
     },
     {
       name :"Shipping Info",
-      Component:()=><div>Enter your shipping address.</div>
+      Component:()=><div className="text-comp">Enter your shipping address.</div>
     },
     {
       name :"Payment",
-      Component:()=><div>Complete payment for your order.</div>
+      Component:()=><div  className="text-comp">Complete payment for your order.</div>
     },
     {
       name :"Delivered ",
-      Component:()=><div>Your order has been delivered.</div>
+      Component:()=><div className="text-comp">Your order has been delivered.</div>
     }
   ]
 
--- stepper/src/components/CheckOutStepper/CheckOutStepper.jsx
+++ stepper/src/components/CheckOutStepper/CheckOutStepper.jsx
@@ -1,26 +1,85 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useRef, useState } from "react";
 
 /* eslint-disable react/prop-types */
 const CheckOutStepper = ({ stepConfig = [] }) => {
-    const [currentStep,setCurrentStep] =useState(1)
-    const [isComplete,setIsComplete] =useState(false)
+  const [currentStep, setCurrentStep] = useState(1);
+  const [isComplete, setIsComplete] = useState(false);
+  const [margins,setMargins] = useState({
+        marginLeft:0,
+        marginRight:0,
+  });
+  const stepRef = useRef([])
+  useEffect(()=>{
+    setMargins({
+        marginLeft:stepRef.current[0].offsetWidth/2,
+        marginRight:stepRef.current[stepConfig.length - 1].offsetWidth/2,
+    })
+        // console.log(stepRef.current[stepConfig.length - 1].offsetWidth);
+
+  },[stepRef,stepConfig.length])
   if (!stepConfig.length) {
     return <></>;
   }
+
+
+  const handleNext = () => {
+    setCurrentStep((prevStep) => {
+      if (prevStep === stepConfig.length) {
+        setIsComplete(true);
+        return prevStep;
+      } else {
+        return prevStep + 1;
+      }
+    });
+  };
+  
+  const calculateProgressBarWidth =()=>{
+    return ((currentStep-1)/(stepConfig.length - 1)) *100;
+  }
+  const ActiveComponent = stepConfig[currentStep - 1]?.Component;
   return (
     <>
-    <div className="stepper">
-      {stepConfig.map((step, index) => {
-        return (
-          <div key={step.name} className="step">
-            <div className="step-number">{index + 1}</div>
-            <div className="step-name">{step.name}</div>
-          </div>
-        );
-      })}
-    </div>
+      <div className="stepper">
+        {stepConfig.map((step, index) => {
+          return (
+            <div
+              key={step.name}
+              ref={(e1)=>(stepRef.current[index] = e1)}
+              className={`step ${
+                currentStep > index + 1 || isComplete ? "complete" : ""
+              }
+            ${currentStep === index + 1 ? "active" : ""}`}
+            >
+              <div className="step-number">
+                {currentStep > index + 1 || isComplete ? (
+                  <span>&#10003;</span>
+                ) : (
+                  index + 1
+                )}
+              </div>
+              <div className="step-name">{step.name}</div>
+            </div>
+          );
+        })}
 
-    <button>Next</button>
+        <div className="progress-bar"
+        style={{
+            width:`calc(100% - ${margins.marginLeft + margins.marginRight}px)`,
+            marginLeft:margins.marginLeft,
+            marginRight:margins.marginRight,
+        }}
+        >
+        
+          <div className="progress" style={{width:`${calculateProgressBarWidth()}%`}} ></div>
+        </div>
+      </div>
+      <ActiveComponent />
+      {!isComplete && (
+        <button className="btn" onClick={handleNext}>
+          {currentStep === stepConfig.length ? "Finish" : "Next"}
+        </button>
+      )}
     </>
   );
 };
